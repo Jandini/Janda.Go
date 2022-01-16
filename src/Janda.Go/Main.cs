948d239176dc69420dc3477c170bea562c6cd33d
Matt Janda <matt.janda.kingston@gmail.com>
2022-01-16 12:13:48 +0000

    Add serilog logging

--- src/Janda.Go/Main.cs
+++ src/Janda.Go/Main.cs
@@ -1,10 +1,19 @@
-﻿namespace Janda.Go
+﻿using Microsoft.Extensions.Logging;
+
+namespace Janda.Go
 {
     internal class Main : IMain
     {
+        readonly ILogger<Main> _logger;
+
+        public Main(ILogger<Main> logger)
+        {
+            _logger = logger;
+        }
+
         public void Run()
         {
-            Console.WriteLine("Hello, World!");
+            _logger.LogInformation("Hello, World!");
         }
     }
 }
--- src/Janda.Go/Program.cs
+++ src/Janda.Go/Program.cs
@@ -1,9 +1,14 @@
 ﻿using Janda.Go;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
 
 new ServiceCollection()
     .AddTransient<IMain, Main>()
+    .AddLogging(builder => builder
+        .AddSerilog(new LoggerConfiguration()
+            .WriteTo.Console(theme: AnsiConsoleTheme.Code, outputTemplate: $"{{Message,-{Console.WindowWidth - 18}:lj}} {{Timestamp:HH:mm:ss}} [ {{Level:u4}} ]{{NewLine}}{{Exception}}")
+            .CreateLogger(), dispose: true))
     .BuildServiceProvider()
     .GetRequiredService<IMain>()
     .Run();
-
