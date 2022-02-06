7bea2e1845c3d27ab50458bbfce9132de290fb73
Matt Janda <matt.janda.kingston@gmail.com>
2022-02-06 15:44:14 +0000

    Remove top level statements (#24)
    * Add test script
    
    * Remove top level statement and make template more explicit

--- src/Console/src/ConsoleGo/Extensions.cs
+++ src/Console/src/ConsoleGo/Extensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Reflection;
 
 namespace ConsoleGo
--- src/Console/src/ConsoleGo/Program.cs
+++ src/Console/src/ConsoleGo/Program.cs
@@ -3,86 +3,102 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 #if (allFeatures)
 using Microsoft.Extensions.Configuration;
-using CommandLine;
+#endif
+using System;
+#if (allFeatures)
+using System.IO;
 #endif
 #if (useSerilog || allFeatures)
 using Serilog;
 #endif
-using ConsoleGo;
-
 #if (allFeatures)
-try
+using CommandLine;
+#endif
+
+namespace ConsoleGo
 {
-    Parser.Default.ParseArguments<Options.Say, Options.Go>(args)
-        .WithParsed((parameters) =>
-        {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddAppSettingsJson("appsettings.json")
-                .Build();
-
-            using var provider = new ServiceCollection()
-                .AddServices()
-                .AddConfiguration(configuration)
-                .AddLogging(builder => builder
-                    .AddSerilog(new LoggerConfiguration()
-                        .ReadFrom.Configuration(configuration)
-                        .CreateLogger(), dispose: true))
-                .BuildServiceProvider();
-
-            provider.LogVersion();
-
+    class Program
+    {
+#if (allFeatures)
+        static void Main(string[] args)
+        {            
             try
             {
-                var main = provider.GetRequiredService<IMain>();
+                Parser.Default.ParseArguments<Options.Say, Options.Go>(args)
+                    .WithParsed((parameters) =>
+                    {
+                        var configuration = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddAppSettingsJson("appsettings.json")
+                            .Build();
 
-                switch (parameters)
+                        using var provider = new ServiceCollection()
+                            .AddServices()
+                            .AddConfiguration(configuration)
+                            .AddLogging(builder => builder
+                                .AddSerilog(new LoggerConfiguration()
+                                    .ReadFrom.Configuration(configuration)
+                                    .CreateLogger(), dispose: true))
+                            .BuildServiceProvider();
+
+                        provider.LogVersion();
+
+                        try
+                        {
+                            var main = provider.GetRequiredService<IMain>();
+
+                            switch (parameters)
+                            {
+                                case Options.Say:
+                                    main.Run();
+                                    break;
+
+                                case Options.Go options:
+                                    main.Go(options.Name);
+                                    break;
+
+                            };
+                        }
+                        catch (Exception ex)
+                        {
+                            provider.GetService<ILogger<Program>>()?
+                                .LogCritical(ex, ex.Message);
+                        }
+                    });
+#else
+        static void Main()
+        {
+            try
+            {
+                using var provider = new ServiceCollection()
+                    .AddTransient<IMain, Main>()
+#if (useSerilog)
+                    .AddLogging(builder => builder
+                    .AddSerilog(new LoggerConfiguration()
+                        .WriteTo.Console()
+                        .CreateLogger(), dispose: true))
+#else
+                    .AddLogging(builder => builder.AddConsole())
+#endif
+                    .BuildServiceProvider();
+
+                try
                 {
-                    case Options.Say:
-                        main.Run();
-                        break;
-
-                    case Options.Go options:
-                        main.Go(options.Name);
-                        break;
-
-                };
+                    provider
+                        .GetRequiredService<IMain>()
+                        .Run();
+                }
+                catch (Exception ex)
+                {
+                    provider.GetRequiredService<ILogger<Program>>()
+                        .LogCritical(ex, ex.Message);
+                }
+#endif
             }
             catch (Exception ex)
             {
-                provider.GetService<ILogger<Program>>()?
-                    .LogCritical(ex, ex.Message);
+                Console.WriteLine(ex.Message);
             }
-        });
-#else
-try
-{
-    using var provider = new ServiceCollection()
-        .AddTransient<IMain, Main>()
-#if (useSerilog)
-        .AddLogging(builder => builder
-            .AddSerilog(new LoggerConfiguration()
-                .WriteTo.Console()
-                .CreateLogger(), dispose: true))
-#else
-        .AddLogging(builder => builder.AddConsole())
-#endif
-        .BuildServiceProvider();
-
-    try
-    {
-        provider
-            .GetRequiredService<IMain>()
-            .Run();
+        }
     }
-    catch (Exception ex)
-    {
-        provider.GetRequiredService<ILogger<Program>>()
-            .LogCritical(ex, ex.Message);
-    }
-#endif
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
 }
