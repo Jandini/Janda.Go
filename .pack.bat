1d5d1e877c8a068142fc497e5c804e25121a537d
Matt Janda <matt.janda.kingston@gmail.com>
2022-11-22 19:50:30 +0000

    Remove constraints

--- .pack.bat
+++ .pack.bat
@@ -7,5 +7,7 @@ for /f %%i in ('dotnet gitversion /showvariable SemVer') do set GIT_VERSION=%%i
 
 nuget pack .nuspec -OutputDirectory bin/Release -NoDefaultExcludes -Version %GIT_VERSION%
 
+echo Uninstalling current version...
 dotnet new uninstall Janda.Go 2>nul
+echo Installing Janda.Go.%GIT_VERSION%
 dotnet new install bin/Release/Janda.Go.%GIT_VERSION%.nupkg
