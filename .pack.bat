5a080b111a229a1e5138590949219eb8b1fca54c
Matt Janda <matt.janda.kingston@gmail.com>
2022-11-22 08:13:03 +0000

    Create build for dotnet 7

--- .pack.bat
+++ .pack.bat
@@ -7,5 +7,5 @@ for /f %%i in ('dotnet gitversion /showvariable SemVer') do set GIT_VERSION=%%i
 
 nuget pack .nuspec -OutputDirectory bin/Release -NoDefaultExcludes -Version %GIT_VERSION%
 
-dotnet new -u Janda.Go 2>nul
-dotnet new -i bin/Release/Janda.Go.%GIT_VERSION%.nupkg
+dotnet new uninstall Janda.Go 2>nul
+dotnet new install bin/Release/Janda.Go.%GIT_VERSION%.nupkg
